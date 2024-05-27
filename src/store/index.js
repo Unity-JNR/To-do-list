5ef45ee10dee30450d47810aa928c281f4af0632
UnityDemas <98840485+UnityDemas@users.noreply.github.com>
2024-05-27 20:16:28 +0200

    added links

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,8 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+const user = 'https://to-do-list-5sqk.onrender.com/users'
+const login ='https://to-do-list-5sqk.onrender.com/login'
+const web = 'https://to-do-list-5sqk.onrender.com/tasks'
 
 export default createStore({
   state: {
