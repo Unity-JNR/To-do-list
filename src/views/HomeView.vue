e1001c0a384f51161d47a9c0f260ac88a7b45726
Unity-JNR <unitydemas12@gmail.com>
2024-05-27 11:49:22 +0200

    Add input and colour scheme

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,7 +8,8 @@
   </div>
 
   <div class="container">
-    <!-- Your content here -->
+    <input type="text" name="text" placeholder="Enter your tasks" class="input">
+    
   </div>
 </template>
 
@@ -26,7 +27,7 @@ export default {
   padding: 20px;
   background-color: #f4e1d2; /* Cream */
   border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); */
   max-width: 400px;
   margin: 20px; /* Add margin to position the box from the top left */
   position: absolute; /* Position the box absolutely */
@@ -52,5 +53,28 @@ export default {
   width: 100%;
   max-width: 400px;
   /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); */
+}
+.input[type = "text"] {
+  display: block;
+  color: rgb(34, 34, 34);
+  background: linear-gradient(142.99deg, rgba(217, 217, 217, 0.63) 15.53%, rgba(243, 243, 243, 0.63) 88.19%);
+  box-shadow: 0px 12px 24px -1px rgba(0, 0, 0,0.18);
+  border-color: rgba(7, 4, 14, 0);
+  border-radius: 50px;
+  block-size: 20px;
+  margin: 7px auto;
+  padding: 18px 15px;
+  outline: none;
+  text-align: center;
+  width: 200px;
+  transition: 0.5s;
+}
+
+.input[type = "text"]:hover {
+  width: 240px;
+}
+
+.input[type = "text"]:focus {
+  width: 280px;
 }
 </style>
